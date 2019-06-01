2ff5de5f134ce5ac7121d280d9d4be83796dd95a
pc <365165904@qq.com>
2019-06-01 16:31:11 +0800

    实体类增加json时间格式注解

--- src/main/java/com/mylike/entity/Anonymity.java
+++ src/main/java/com/mylike/entity/Anonymity.java
@@ -1,5 +1,7 @@
 package com.mylike.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -9,7 +11,7 @@ public class Anonymity implements Serializable {
     private String aName;
 
     private String aHendpic;
-
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date aAddtime;
 
     public Integer getaId() {
